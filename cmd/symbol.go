f13f8684639e9fd06d0d3f397c1461cbb6f9d65c
mattak <mattak.me@gmail.com>
2021-02-11 21:23:31 +0900

    :seedling: add tokyo symbol fetch

--- cmd/symbol.go
+++ cmd/symbol.go
@@ -5,16 +5,19 @@ import (
 	"github.com/gocolly/colly"
 	"github.com/spf13/cobra"
 	"os"
+	"regexp"
 	"sort"
 )
 
 var (
 	SymbolCmd = &cobra.Command{
-		Use:     "symbol",
-		Short:   "List up symbols",
-		Long:    `List up symbols of NASDAQ`,
-		Example: `  stonk symbol`,
-		Run:     runCommandSymbol,
+		Use:   "symbol",
+		Short: "List up symbols",
+		Long:  `List up symbols of NASDAQ`,
+		Example: `  stonk symbol
+  stonk symbol nasdaq
+  stonk symbol tokyo`,
+		Run: runCommandSymbol,
 	}
 )
 
@@ -23,7 +26,16 @@ func init() {
 
 func runCommandSymbol(cmd *cobra.Command, args []string) {
 	tickerMapChannel := make(chan map[string]bool)
-	go fetchTickers(tickerMapChannel)
+	marketType := "nasdaq"
+	if len(args) >= 1 {
+		marketType = args[0]
+	}
+
+	if marketType == "tokyo" {
+		go fetchTokyoTickers(tickerMapChannel)
+	} else {
+		go fetchNasdaqTickers(tickerMapChannel)
+	}
 
 	tickerMap := <-tickerMapChannel
 	tickers := []string{}
@@ -40,8 +52,56 @@ func printSymbols(list []string) {
 	}
 }
 
+func fetchTokyoTickers(tickerMapChannel chan map[string]bool) {
+	codes := map[string]bool{}
+	page := 1
+	re := regexp.MustCompile(`code=([\d\.\w]+)`)
+	c := colly.NewCollector()
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		e.ForEach("div#contents-body-bottom table.rankingTable tbody tr", func(y int, e *colly.HTMLElement) {
+			line := []string{}
+			e.ForEach("td a", func(x int, e *colly.HTMLElement) {
+				link := e.Attr("href")
+				if len(link) > 0 {
+					result := re.FindAllStringSubmatch(link, -1)
+					if len(result) > 0 && len(result[0]) > 1 {
+						codes[result[0][1]] = true
+					}
+				}
+				line = append(line, e.Text)
+			})
+		})
+
+		pageLinks := []string{}
+		e.ForEach("div#contents-body-bottom ul.ymuiPagingBottom a", func(y int, e *colly.HTMLElement) {
+			pageLinks = append(pageLinks, e.Text)
+		})
+
+		if pageLinks[len(pageLinks)-1] == "次へ" {
+			page++
+			url := fmt.Sprintf("https://info.finance.yahoo.co.jp/ranking/?kd=4&tm=d&vl=a&mk=1&p=%d", page)
+			err := e.Request.Visit(url)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			tickerMapChannel <- codes
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Fprintln(os.Stderr, "Visiting", r.URL.String())
+	})
+
+	err := c.Visit(fmt.Sprintf("https://info.finance.yahoo.co.jp/ranking/?kd=4&tm=d&vl=a&mk=1&p=%d", page))
+	if err != nil {
+		panic(err)
+	}
+}
+
 // fetch nasdaq symbols
-func fetchTickers(tickerMapChannel chan map[string]bool) {
+func fetchNasdaqTickers(tickerMapChannel chan map[string]bool) {
 	tickerMap := map[string]bool{}
 	c := colly.NewCollector()
 	pages := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
