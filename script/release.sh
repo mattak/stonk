89ec3bfd49cac4d67816959bd2cab480043a2975
mattak <mattak.me@gmail.com>
2021-05-14 09:18:20 +0900

    :seedling: add build check on reelase script

--- script/release.sh
+++ script/release.sh
@@ -1,32 +1,52 @@
 #!/bin/sh
 
+check_branch() {
+  BRANCH=$(git branch | grep '*' | awk '{print $2}')
+  if [ $BRANCH != "master" ]; then
+    echo "branch is not master"
+    exit 1
+  fi
+}
+
+check_version_exists() {
+  version=$1
+  if git tag | grep $version >/dev/null; then
+    echo "ERROR: $version already exists in git-tag list"
+    exit 1
+  fi
+}
+
+update_version_file() {
+  cat <<__TEXT__ > cmd/version.go
+package cmd
+
+var VERSION = "$version"
+__TEXT__
+}
+
+
+test_and_build_check() {
+  make test
+  make && make install
+}
+
+push_and_commit() {
+  git add cmd/version.go
+  git commit -m ":up: bump up $version"
+  git tag $version
+  git push origin
+  git push origin --tags
+}
+
 if [ $# -ne 1 ]; then
   echo "usage: <version>"
   exit 1
 fi
 
-BRANCH=$(git branch | grep '*' | awk '{print $2}')
-if [ $BRANCH != "master" ]; then
-  echo "branch is not master"
-  exit 1
-fi
-
 version=$1
+check_branch
+check_version_exists $version
+update_version_file
+test_and_build_check
+push_and_commit
 
-if git tag | grep $version >/dev/null; then
-  echo "ERROR: $version already exists in git-tag list"
-  exit 1
-fi
-
-cat <<__TEXT__ > cmd/version.go
-package cmd
-
-var VERSION = "$version"
-__TEXT__
-
-
-git add cmd/version.go
-git commit -m ":up: bump up $version"
-git tag $version
-git push origin
-git push origin --tags
